d3e5367f959b1fda5d7300e95766d7767111ab1b
James Napier <jamesnapier64@gmail.com>
2023-08-21 23:05:03 +1200

    fix: Incorrect input font size

--- src/lib/components/QuckTakehome.svelte
+++ src/lib/components/QuckTakehome.svelte
@@ -26,12 +26,12 @@
 				<input
 					type="number"
 					bind:value={income.pay}
-					class="text-2xl py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
+					class="t-2xl py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
 				/>
 				per
 				<select
 					bind:value={income.period}
-					class="text-2xl py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
+					class="t-2xl py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
 				>
 					<option value="hour">hour</option>
 					<option value="week">week</option>
@@ -43,7 +43,7 @@
 				<input
 					type="number"
 					bind:value={income.kiwiSaver}
-					class="text-2xl py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
+					class="t-2xl py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
 				/>
 				% to kiwisaver, with
 				<label
@@ -105,3 +105,9 @@
 		</div>
 	</div>
 </main>
+
+<style>
+	.t-2xl {
+		@apply text-2xl;
+	}
+</style>
